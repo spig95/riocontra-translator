901bb1759bfc663d7ef6ad1c770ab95835d46182
spig95 <nicola.ischia@schindler.com>
2021-04-10 19:11:16 +0200

    Load header and footer from separate htmls

---
+++ js/essentials.js
@@ -0,0 +1,7 @@
+// Each html page should include this script
+
+// Load header and footer
+$(function(){
+    $("#header").load("../html/header.html"); 
+    $("#footer").load("../html/footer.html"); 
+});
