f993e704e4d7111d6c69a4f7cdb2abfeff7ffe9c
spig95 <nicolaischia95@gmail.com>
2023-04-23 14:22:16 +0200

    Fix links for github pages

--- js/load_rules_modals.js
+++ js/load_rules_modals.js
@@ -2,5 +2,5 @@ $(() => {
 	const div = document.createElement('div');
 	div.id = 'rules-modals-placeholder';
 	$('body').append(div);
-	$('#rules-modals-placeholder').load('../html/chunks/rules_modals.html');
+	$('#rules-modals-placeholder').load('/riocontra-translator/html/chunks/rules_modals.html');
 });
